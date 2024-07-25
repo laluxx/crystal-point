0190713ece35893aedf745c4414426f4b3b02968
laluxx <lorenzoaddamo10@gmail.com>
2024-07-25 15:04:01 +0200

    hl-line-mode fix

--- crystal-point.el
+++ crystal-point.el
@@ -9,9 +9,12 @@
 (defun crystal-point/update-cursor-color ()
   "Update the cursor color based on the foreground color of the character at point."
   (let* ((pos (point))
-         ;; Attempt to find the face using overlays first, then text properties.
-         (face (or (car (face-at-point nil t))  ; Get face from overlays/text properties.
-                   'default))  ; Fallback to default if no face is found.
+         ;; Temporarily disable hl-line-mode if it's enabled.
+         (hl-line-enabled (bound-and-true-p hl-line-mode))
+         (face (progn
+                 (when hl-line-enabled (hl-line-mode -1))
+                 (or (car (face-at-point nil t))  ; Get face from overlays/text properties.
+                     'default)))
          (fg-color (face-attribute face :foreground nil t))
          ;; Use the real cursor color of the theme, or the foreground of `font-lock-comment-face` as fallback.
          (theme-cursor-color (frame-parameter nil 'cursor-color))
@@ -19,6 +22,8 @@
          (cursor-color (if (or (not fg-color) (string= fg-color "unspecified"))
                            (or theme-cursor-color fallback-color)
                          fg-color)))
+    ;; Re-enable hl-line-mode if it was previously enabled.
+    (when hl-line-enabled (hl-line-mode 1))
     ;; Set the cursor color if it differs from the current one to minimize updates.
     (unless (equal cursor-color (frame-parameter nil 'cursor-color))
       (set-cursor-color cursor-color))))
